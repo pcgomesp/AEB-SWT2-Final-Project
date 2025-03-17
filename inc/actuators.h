b4738d1eddb5339bf05e6348ba4d7b53f3ba9ef8
Paulo Gomes <pcesarpgomes@gmail.com>
2025-03-17 13:39:46 -0300

    Fix: Remove unecessary functions and introduces acuators abstraction

--- inc/actuators.h
+++ inc/actuators.h
@@ -3,35 +3,37 @@
 
 #include <stdbool.h>
 
-typedef struct SeatBeltStruct{
-    bool shouldTightenBelt; 
-} SeatBelt; 
+typedef struct{
+    bool belt_tightness;
+    bool door_lock;
+    bool should_activate_abs;
+} Actuators_abstraction;
+
+// typedef struct SeatBeltStruct{
+//     bool shouldTightenBelt; 
+// } SeatBelt; 
 
 typedef enum SeatBeltEnum{
     LOOSE_BELT = 0,
     TIGHT_BELT = 1 
 } StateSeatBelt; 
 
-typedef struct DoorLockStruct{
-    bool shouldUnlockDoor; 
-} DoorLock; 
+// typedef struct DoorLockStruct{
+//     bool shouldUnlockDoor; 
+// } DoorLock; 
 
 typedef enum DoorLockEnum{
     UNLOCKED_DOOR = 0,
     LOCKED_DOOR = 1 
 } StateDoorLock;
 
-typedef struct ABSStruct{
-    bool shouldActivateBreak; 
-} ABS; 
+// typedef struct ABSStruct{
+//     bool shouldActivateBreak; 
+// } ABS; 
 
 typedef enum ABSEnum{
     ABS_DEACTIVATED = 0,
     ABS_ACTIVATED = 1
-} StateABS; 
-
-StateSeatBelt getSeatBeltState(SeatBelt seatBelt);
-StateDoorLock getDoorLockState(DoorLock doorLock);
-StateABS getABSState(ABS abs);
+} StateABS;
 
 #endif
