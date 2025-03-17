83722576e28aef3ea2a8eaaf06c86d96d2b97f58
Paulo Gomes <pcesarpgomes@gmail.com>
2025-03-17 09:47:08 -0300

    Feat: Basic DBC File

---
+++ inc/dbc.h
@@ -0,0 +1,16 @@
+#ifndef DBC_H
+#define DBC_H
+
+#define ID_PEDALS       0x18FEF100
+#define ID_SPEED_S      0x18FFFD64
+#define ID_OBSTACLE_S   0x0CFFB027
+#define ID_CAR_C        0x0CFFAF27
+#define ID_AEB_S        0x18FFA027
+
+#define MAX_SPEED_S     251
+#define MAX_OBSTACLE_S  150
+
+#define RES_SPEED_S     1/256
+#define RES_OBSTACLE_S  1/20
+
+#endif
