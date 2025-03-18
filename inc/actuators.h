0a38e866cb51fb6ee033005e018f717f6010725b
Paulo Gomes <pcesarpgomes@gmail.com>
2025-03-17 21:14:23 -0300

    Feat & Fix: Adjust actuators and sensors abstraction

--- inc/actuators.h
+++ inc/actuators.h
@@ -7,33 +7,8 @@ typedef struct{
     bool belt_tightness;
     bool door_lock;
     bool should_activate_abs;
-} Actuators_abstraction;
-
-// typedef struct SeatBeltStruct{
-//     bool shouldTightenBelt; 
-// } SeatBelt; 
-
-typedef enum SeatBeltEnum{
-    LOOSE_BELT = 0,
-    TIGHT_BELT = 1 
-} StateSeatBelt; 
-
-// typedef struct DoorLockStruct{
-//     bool shouldUnlockDoor; 
-// } DoorLock; 
-
-typedef enum DoorLockEnum{
-    UNLOCKED_DOOR = 0,
-    LOCKED_DOOR = 1 
-} StateDoorLock;
-
-// typedef struct ABSStruct{
-//     bool shouldActivateBreak; 
-// } ABS; 
-
-typedef enum ABSEnum{
-    ABS_DEACTIVATED = 0,
-    ABS_ACTIVATED = 1
-} StateABS;
+    bool alarm_led;
+    bool alarm_buzzer;
+} actuators_abstraction;
 
 #endif
