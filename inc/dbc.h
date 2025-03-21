9bb16caf5712dc4addc636f3af29396d9f746374
Paulo Gomes <pcesarpgomes@gmail.com>
2025-03-20 21:22:37 -0300

    Feat: creates can_msg conversion functions in 'sensors module'

--- inc/dbc.h
+++ inc/dbc.h
@@ -1,20 +1,27 @@
 #ifndef DBC_H
 #define DBC_H
 
-#define ID_PEDALS       0x18FEF100
-#define ID_SPEED_S      0x18FFFD64
-#define ID_OBSTACLE_S   0x0CFFB027
+//#define ID_PEDALS       0x18FEF100
+//#define ID_SPEED_S      0x18FFFD64
+//#define ID_OBSTACLE_S   0x0CFFB027
 //#define ID_CAR_C        0x0CFFAF27
-#define ID_CAR_C        {0x0C, 0xFF, 0xAF, 0x27}
-#define ID_AEB_S        0x18FFA027
+//#define ID_AEB_S        0x18FFA027
 
+// Identifiers, according to the dbc file in the requirements specification
+#define ID_PEDALS       {0x18, 0xFE, 0xF1, 0x00}
+#define ID_SPEED_S      {0x18, 0xFF, 0xFD, 0x64}
+#define ID_OBSTACLE_S   {0x0C, 0xFF, 0xB0, 0x27}
+#define ID_CAR_C        {0x0C, 0xFF, 0xAF, 0x27}
+#define ID_AEB_S        {0x18, 0xFF, 0xA0, 0x27}
+
+// left most: least significant // right most: most significant
 #define BASE_DATA_FRAME {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
 
 #define MAX_SPEED_S     251
 #define MAX_OBSTACLE_S  150
 
-#define RES_SPEED_S     1/256
-#define RES_OBSTACLE_S  1/20
+#define RES_SPEED_S     (1.0/256.0)
+#define RES_OBSTACLE_S  (1.0/20.0)
 
 // Estrutura da mensagem estilo CAN a ser enviada para a fila
 typedef struct {
--- src/sensors.c
+++ src/sensors.c
@@ -17,9 +17,9 @@ can_msg conv2CANPedalsData(bool brake_pedal, bool accelerator_pedal);
 pthread_t sensors_id;
 char *shm_ptr;
 sensors_input_data sensorsData = {
-    .vehicle_velocity = 0.0, 
+    .vehicle_velocity = 50.0, 
     .has_obstacle = false, 
-    .obstacle_distance = 0.0, 
+    .obstacle_distance = 90.0, 
     .brake_pedal = false, 
     .accelerator_pedal = false, 
     .on_off_aeb_system = true
@@ -68,28 +68,95 @@ void* getSensorsData(void *arg){
 
     // Part 2: Convert data to can_msg format
     can_car_cluster     = conv2CANCarClusterData(sensorsData.on_off_aeb_system);
-    // can_velocity_sensor = conv2CANVelocityData();
-    // can_obstacle_sensor = conv2CANObstacleData();
-    // can_pedals_sensor   = conv2CANPedalsData();
-
+    can_velocity_sensor = conv2CANVelocityData(sensorsData.vehicle_velocity);
+    can_obstacle_sensor = conv2CANObstacleData(sensorsData.has_obstacle, sensorsData.obstacle_distance);
+    can_pedals_sensor   = conv2CANPedalsData(sensorsData.brake_pedal, sensorsData.accelerator_pedal);
 
     // Part 3: Send all four frames to sensors_mq
     
     
     
     // Part 4: simple sleep timer here;
+    
 
+    // PlaceHolder: just some simple prints to see if the data is being converted in the right way
     print_can_msg(&can_car_cluster);
+    print_can_msg(&can_velocity_sensor);
+    print_can_msg(&can_obstacle_sensor);
+    print_can_msg(&can_pedals_sensor);
 
     printf("Placeholder\n");
     return NULL;
 }
 
+// The location of information in the data frame location, in the following functions, 
+// is according to the dbc file in the requirements specification
+
 can_msg conv2CANCarClusterData(bool on_off_aeb_system){
     can_msg aux = {.identifier = ID_CAR_C, .dataFrame = BASE_DATA_FRAME};
 
+    if(on_off_aeb_system){
+        aux.dataFrame[0] = 0x01;
+    }
+    else{
+        aux.dataFrame[0] = 0x00;
+    }
+
     return aux;
 }
-// can_msg conv2CANVelocityData(double vehicle_velocity);
-// can_msg conv2CANObstacleData(bool has_obstacle, double obstacle_distance);
-// can_msg conv2CANPedalsData(bool brake_pedal, bool accelerator_pedal);
+
+can_msg conv2CANVelocityData(double vehicle_velocity){
+    can_msg aux = {.identifier = ID_SPEED_S, .dataFrame = BASE_DATA_FRAME};
+
+    // Speed data ​​encapsulation: Data_speed = speed/res - offset => speed * 256
+    unsigned int data_speed = vehicle_velocity / RES_SPEED_S;
+    unsigned char ms_speed, ls_speed;
+    ls_speed = data_speed;
+    ms_speed = data_speed >> 8;
+
+    // Defines most and least significant bytes, according to the DBC specification
+    aux.dataFrame[0] = ls_speed;
+    aux.dataFrame[1] = ms_speed;
+
+    return aux;
+}
+
+can_msg conv2CANObstacleData(bool has_obstacle, double obstacle_distance){
+    can_msg aux = {.identifier = ID_OBSTACLE_S, .dataFrame = BASE_DATA_FRAME};
+
+    if(has_obstacle){
+        aux.dataFrame[2] = 0x01;
+    } else {
+        aux.dataFrame[2] = 0x00;
+    }
+
+    // Obstacle distance data ​​encapsulation: Data_speed = speed/res - offset => speed * 256
+    unsigned int data_distance = obstacle_distance / RES_OBSTACLE_S;
+    unsigned char ms_distance, ls_distance;
+    ls_distance = data_distance;
+    ms_distance = data_distance >> 8;
+
+    // Defines most and least significant bytes, according to the DBC specification
+    aux.dataFrame[0] = ls_distance;
+    aux.dataFrame[1] = ms_distance;
+
+    return aux;
+}
+
+can_msg conv2CANPedalsData(bool brake_pedal, bool accelerator_pedal){
+    can_msg aux = {.identifier = ID_PEDALS, .dataFrame = BASE_DATA_FRAME};
+
+    if(brake_pedal){
+        aux.dataFrame[1] = 0x01;
+    } else {
+        aux.dataFrame[1] = 0x00;
+    }
+
+    if(accelerator_pedal){
+        aux.dataFrame[0] = 0x01;
+    } else {
+        aux.dataFrame[0] = 0x00;
+    }
+
+    return aux;
+}
