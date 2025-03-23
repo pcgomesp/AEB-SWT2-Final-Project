36684c1cf8567522b47f1b93474a097dce92978a
Rafael Bernardo <rafaelbnneto@gmail.com>
2025-03-23 17:13:25 -0300

    test(test_mq_utils_read.c): fix test to adapt to new can_msg identifier

--- test/test_mq_utils_read.c
+++ test/test_mq_utils_read.c
@@ -47,22 +47,21 @@ void test_read_and_write_mq_empty_can_msg()
     write_mq(mq_write, &msg_to_write);
     can_msg msg_read;
     read_mq(mqd, &msg_read);
-    TEST_ASSERT_EQUAL(msg_to_write.identifier[0], msg_read.identifier[0]);
+    TEST_ASSERT_EQUAL(msg_to_write.identifier, msg_read.identifier);
 }
 
 void test_read_and_write_mq_valid_can_msg()
 {
     mqd_t mq_write = open_mq(mq_name);
     can_msg msg = {
-        .identifier = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08},
-        .dataFrame = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}
-    };
+        .identifier = 12345,
+        .dataFrame = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}};
     write_mq(mq_write, &msg);
     can_msg msg_read;
     read_mq(mqd, &msg_read);
+    TEST_ASSERT_EQUAL(msg.identifier, msg_read.identifier);
     for (int i = 0; i < 8; i++)
     {
-        TEST_ASSERT_EQUAL(msg.identifier[i], msg_read.identifier[i]);
         TEST_ASSERT_EQUAL(msg.dataFrame[i], msg_read.dataFrame[i]);
     }
 }
