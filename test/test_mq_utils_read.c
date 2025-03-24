993d5df55d231a95950ca2d1c81459002a666662
Rafael Bernardo <rafaelbnneto@gmail.com>
2025-03-24 15:47:16 -0300

    style: format test_mq_utils_read.c

--- test/test_mq_utils_read.c
+++ test/test_mq_utils_read.c
@@ -45,24 +45,28 @@ void test_read_and_write_mq_empty_can_msg()
     mqd_t mq_write = open_mq(mq_name);
     can_msg msg_to_write = {0};
     write_mq(mq_write, &msg_to_write);
+
     can_msg msg_read;
     read_mq(mqd, &msg_read);
+
     TEST_ASSERT_EQUAL(msg_to_write.identifier, msg_read.identifier);
 }
 
 void test_read_and_write_mq_valid_can_msg()
 {
     mqd_t mq_write = open_mq(mq_name);
-    can_msg msg = {
+    can_msg msg_to_write = {
         .identifier = 12345,
         .dataFrame = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}};
-    write_mq(mq_write, &msg);
+    write_mq(mq_write, &msg_to_write);
+
     can_msg msg_read;
     read_mq(mqd, &msg_read);
-    TEST_ASSERT_EQUAL(msg.identifier, msg_read.identifier);
+
+    TEST_ASSERT_EQUAL(msg_to_write.identifier, msg_read.identifier);
     for (int i = 0; i < 8; i++)
     {
-        TEST_ASSERT_EQUAL(msg.dataFrame[i], msg_read.dataFrame[i]);
+        TEST_ASSERT_EQUAL(msg_to_write.dataFrame[i], msg_read.dataFrame[i]);
     }
 }
 
